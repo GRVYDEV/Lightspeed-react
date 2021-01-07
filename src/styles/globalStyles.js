0fc0f4949bee0103901e9857c48c4640714c3de8
razor9876 <kamil.sienkiewicz1997@gmail.com>
2021-01-07 23:55:06 +0100

    Moving css to styled components

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        padding: 0;
+        margin:0;
+        border: 0;
+        font-family: 'Poppins', sans-serif;
+        font-style: normal;
+    }
+
+    body{
+        background-color: #1f2128;
+    }
+
+    .App{
+        text-align:center;
+    }
+`;
+
+export default GlobalStyle;
